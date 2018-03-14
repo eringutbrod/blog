d390025abefcb4546f0f598be13c654cd4aad6b6
eringutbrod <elg82@cornell.edu>
2018-03-14 12:48:55 -0700

    1st post

---
+++ blogdown_tutorial.R
@@ -0,0 +1,13 @@
+#Eco-data-science blogdown tutorial led by Jamie Afflerbach
+
+#install.packages("blogdown")
+library(blogdown)
+install_hugo()  #https://themes.gohugo.io/ (website themes)
+
+new_site(theme="gcushen/hugo-academic")
+
+
+
+
+
+
